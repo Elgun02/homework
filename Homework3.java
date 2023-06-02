aebba578ee61a94c0a8e4936704f14fef1beedec
Elgun <132170610+Elgun02@users.noreply.github.com>
2023-06-02 12:51:24 +0300

    Homework3

---
+++ Homework3.java
@@ -0,0 +1,22 @@
+package Homework;
+
+public class Homework3 {
+    public static void main(String[] args) {
+
+        char a = '0';
+        char b = '0';
+        char c = '0';
+
+        // ASCII table
+        System.out.println((int) a);
+
+        // String valueOf()
+        int bb = Integer.parseInt(String.valueOf(b));
+        System.out.println(bb);
+
+        // getNumericValue()
+        int cc = Character.getNumericValue(c);
+        System.out.println(cc);
+        
+    }
+}
